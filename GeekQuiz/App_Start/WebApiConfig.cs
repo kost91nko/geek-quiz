6cd40366f9269ac04d3b1e7c8032bf41abfa9488
Artemov Konstantin <kost91nko@gmail.com>
2015-06-01 18:44:05 +0300

    Setup Trivia model functional

--- GeekQuiz/App_Start/WebApiConfig.cs
+++ GeekQuiz/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using Newtonsoft.Json.Serialization;
+
 namespace GeekQuiz
 {
     public static class WebApiConfig
@@ -10,6 +12,9 @@ namespace GeekQuiz
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // Use camel case for JSON data
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver()
 
             // Web API routes
             config.MapHttpAttributeRoutes();
